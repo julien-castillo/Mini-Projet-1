2795587673fde4212a08eb32257d8af70c4c4a87
Julien Castillo <juliencastillo77@gmail.com>
2021-11-02 17:18:58 +0100

    End of Live n°4, add forEach function

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,13 @@
-const panels = document.querySelectorAll('.container > *');
+const panels = document.querySelectorAll(".container > *");
 
-panels[0].addEventListener('click', function() {
-    console.log("yeah 3");
-});
+for (let panel of panels) {
+    panel.addEventListener('click', function() {
+        panels.forEach(p =>p.classList.remove('active'));
+        panel.classList.add('active');
+    });
+};
+
+const panelDiv = document.createElement('div');
+
+panelDiv.style = "background-image: url(/img/explore-the-world.jpg)";
+panelDiv.className = "panel active";
