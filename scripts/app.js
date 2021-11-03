faa729e662acd6194ca562f377928344162e9dfa
Julien Castillo <juliencastillo77@gmail.com>
2021-11-03 12:30:48 +0100

    End of Live n°5 du 12 octobre 2021

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,26 @@
-const panels = document.querySelectorAll(".container > *");
+const images = [
+  "city-on-winter",
+  "explore-the-world",
+  "mountains-and-clouds",
+  "sunny-beach",
+  "wild-forest",
+];
 
-for (let panel of panels) {
-    panel.addEventListener('click', function() {
-        panels.forEach(p =>p.classList.remove('active'));
-        panel.classList.add('active');
-    });
-};
+for (let image of images) {
+  const panel = createPanel(image);
+  panel.addEventListener("click", function () {
+    // panels.forEach(p =>p.classList.remove('active'));
+    panel.classList.add("active");
+  });
+}
 
-const panelDiv = document.createElement('div');
+function createPanel(img) {
+  const panelDiv = document.createElement("div");
 
-panelDiv.style = "background-image: url(/img/explore-the-world.jpg)";
-panelDiv.className = "panel active";
+  panelDiv.style = "background-image: url(img/" + img + ".jpg)";
+  panelDiv.className = "panel";
+  panelDiv.innerHTML = "<h3>MON TITRE</3>";
 
-document.querySelector('.container').appendChild(panelDiv);
+  document.querySelector(".container").appendChild(panelDiv);
+  return panelDiv;
+}
